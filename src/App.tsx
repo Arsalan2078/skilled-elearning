1094e7344f5d261c0b34a243008a9987033a1c98
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-01-26 22:05:01 -0500

    starter files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app text-preset-5">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero unde
+      voluptatibus laboriosam atque placeat quaerat voluptate id, quas amet
+      consequuntur quidem sint laudantium nemo, facere molestias quos, a nulla.
+      Quas?
+    </div>
+  );
+}
+
+export default App;
